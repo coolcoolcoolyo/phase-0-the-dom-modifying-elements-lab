c189ad28cf04c75cbf16d6b7dcd853d931c10945
Alexander Spagnoli <alexjspagnoli@gmail.com>
2022-09-12 03:09:04 -0400

    commit tree c3b5e692278a5aadf692e1cdf741152cff62bdfa

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader)
+
+let name = "Alexander Spagnoli";
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = '${name} is the champion';
